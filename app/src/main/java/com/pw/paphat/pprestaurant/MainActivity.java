e8070420a79ed99c32a3bfdf483e18caf0390c75
NYR <paphat.dev@gmail.com>
2016-04-09 11:57:59 +0700

    Add & Delete Finish

--- app/src/main/java/com/pw/paphat/pprestaurant/MainActivity.java
+++ app/src/main/java/com/pw/paphat/pprestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pw.paphat.pprestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,7 +18,25 @@ public class MainActivity extends AppCompatActivity {
         //Requrest SQLite (want to use database)
         myManage = new MyManage(this);
 
+        //Test Add Value
+        //testAdd();
+
+        //Delete SQLite
+        deleteSQLite();
+
 
 
     }   //Main Method
+
+    private void deleteSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE,null);//null mean non sequirity
+        sqLiteDatabase.delete(MyManage.food_table,null,null);//null null mean delete all talbe
+        sqLiteDatabase.delete(MyManage.user_table,null,null);
+    }
+
+    private void testAdd() {
+        myManage.addValueToSQLite(0, "user", "pass", "name");
+        myManage.addValueToSQLite(1, "food", "price", "source");
+    }
 }   //Main Class
--- app/src/main/java/com/pw/paphat/pprestaurant/MyManage.java
+++ app/src/main/java/com/pw/paphat/pprestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package com.pw.paphat.pprestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,17 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";//userTABLE must name same MyOpenHelper
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_pass = "Password";
+    public static final String column_name = "Name";
+
+    public static final String food_table = "foodTABLE";
+    public static final String column_food = "Food";
+    public static final String column_price = "Price ";
+    public static final String column_source = "Source";//must not explicit id again because use together above table
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
@@ -19,4 +31,42 @@ public class MyManage {
 
 
     }//Constructor
+
+
+    public long addValueToSQLite(int intTABLE,
+                                 String strFirst,
+                                 String strSecond,
+                                 String strThird) {
+
+        ContentValues contentValues = new ContentValues();
+        long myLong = 0;
+
+        switch (intTABLE) {
+
+            case 0:
+                //for userTABLE
+                contentValues.put(column_user, strFirst);
+                contentValues.put(column_pass, strSecond);
+                contentValues.put(column_name, strThird);
+
+//                return sqLiteDatabase.insert(user_table, null, contentValues);
+
+                myLong = sqLiteDatabase.insert(user_table, null, contentValues);
+
+                break;
+            case 1:
+                //for foodTABLE
+                contentValues.put(column_food, strFirst);
+                contentValues.put(column_price, strSecond);
+                contentValues.put(column_source, strThird);
+
+                myLong = sqLiteDatabase.insert(food_table, null, contentValues);
+
+                break;
+        }   //switch
+
+
+        return myLong;
+    }//method send data to database
+
 }// Main Class
